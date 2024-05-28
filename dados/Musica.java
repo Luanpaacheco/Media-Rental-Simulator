d5ae301c439e621d809cc89db649a0c7cadf7733
Luan Pacheco Lima <luanlimapacheco@gmail.com>
2024-05-28 19:40:03 -0300

    fiz o metodo CalculaLocaçao() das subclasses

--- dados/Musica.java
+++ dados/Musica.java
@@ -3,17 +3,24 @@ package dados;
 public class Musica extends Midia {
 
 	private double duracao;
+	private Categoria ca;
 
 	public Musica(int codigo, String titulo, int ano, Categoria categoria, double duracao) {
 			super(codigo, titulo, ano, categoria);
 			this.duracao=duracao;
+			ca=categoria;
 			
 	}
 
 	@Override
 	public double calculaLocacao() {
-		// TODO Auto-generated method stub
-		return 5;
+		if(ca.equals(Categoria.ACA)){
+			return duracao*0.9;
+		}else if (ca.equals(Categoria.DRA)){
+			return duracao*0.7;
+		}else if (ca.equals(Categoria.FIC)){
+			return duracao*0.5;
+		}else return duracao*0.3;
 	}
 
 }
--- dados/Video.java
+++ dados/Video.java
@@ -3,16 +3,21 @@ package dados;
 public class Video extends Midia {
 
 	private int qualidade;
+	private Categoria ca;
 
 	public Video(int codigo, String titulo, int ano, Categoria categoria, int qualidade) {
 		super(codigo, titulo, ano, categoria);
 		this.qualidade=qualidade;
+		ca=categoria;
 	}
 
 	@Override
 	public double calculaLocacao() {
-		// TODO Auto-generated method stub
-		return 5;
+		if(super.getAno()==2024){
+			return 20;
+		}else if (super.getAno()>=2000||super.getAno()<=2023){
+			return 15;
+		}else return 10;
 	}
 
 	
