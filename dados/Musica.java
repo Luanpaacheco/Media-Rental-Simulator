7e4839a8c19f24651a080efc187f4e34658e610e
Luan Pacheco Lima <luanlimapacheco@gmail.com>
2024-05-27 22:59:29 -0300

    colocando o metodo abstrato e ajeitando os super par não dar erro

--- dados/Musica.java
+++ dados/Musica.java
@@ -5,7 +5,15 @@ public class Musica extends Midia {
 	private double duracao;
 
 	public Musica(int codigo, String titulo, int ano, Categoria categoria, double duracao) {
+			super(codigo, titulo, ano, categoria);
+			this.duracao=duracao;
+			
+	}
 
+	@Override
+	public double calculaLocacao() {
+		// TODO Auto-generated method stub
+		return 5;
 	}
 
 }
--- dados/Video.java
+++ dados/Video.java
@@ -5,7 +5,16 @@ public class Video extends Midia {
 	private int qualidade;
 
 	public Video(int codigo, String titulo, int ano, Categoria categoria, int qualidade) {
-
+		super(codigo, titulo, ano, categoria);
+		this.qualidade=qualidade;
 	}
+
+	@Override
+	public double calculaLocacao() {
+		// TODO Auto-generated method stub
+		return 5;
+	}
+
+	
 
 }
